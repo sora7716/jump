a78d676fbcde2ff87eb078f360f4df6e67f6cb68
sora <147121795+sora7716@users.noreply.github.com>
2024-06-20 16:47:52 +0900

    アニメーションを追加しました

--- Assets/Item.cs
+++ Assets/Item.cs
@@ -5,21 +5,42 @@ using UnityEngine;
 
 public class Item : MonoBehaviour
 {
+    Animator _anim;
 
     void Start()
     {
-      
+        _anim = GetComponent<Animator>();
+        _anim.SetBool("IsGet", false);
     }
 
     void Update()
     {
-        
+        //if (Input.GetKeyDown(KeyCode.V))
+        //{
+        //    _anim.Play("Idle");
+        //}
+        //if (Input.GetKeyDown(KeyCode.C))
+        //{
+
+        //}
+    }
+
+    /// <summary>
+    /// 獲得アニメーションの再生が終わったら呼ぶ
+    /// オブジェクトを破棄する
+    /// </summary>
+    void OnGetAnimationFinished()
+    {
+        Debug.Log("アイテムを破棄します");
+        Destroy(this.gameObject);
     }
     private void OnTriggerEnter(Collider other)
     {
+        //_anim.Play("Get");
+        _anim.SetBool("IsGet", true);
         Rigidbody rb = other.GetComponent<Rigidbody>();
         rb.drag = 15;
-       // Destroy(this.gameObject);
+        //Destroy(this.gameObject);
         Debug.Log(other.gameObject.name + "が接触した");
     }
 
